e233a3d4b9d2cf6af51bc25edf16acbc60ccf5ec
Vlad Golubev <golubev.vld@gmail.com>
2015-11-28 21:01:10 +0200

    Initial commit :dizzy:

---
+++ src/ua/vladgolubev/Main.java
@@ -0,0 +1,8 @@
+package ua.vladgolubev;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Algorythms and Data Structures individual term task");
+    }
+}
